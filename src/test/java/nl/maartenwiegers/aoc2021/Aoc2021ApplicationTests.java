d7215b906e1f9f6105838164b517ce74176abac1
Maarten <github@maartenwiegers.nl>
2021-11-29 20:12:49 +0100

    Spring init

---
+++ src/test/java/nl/maartenwiegers/aoc2021/Aoc2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.maartenwiegers.aoc2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aoc2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
