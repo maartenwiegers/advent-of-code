f7776056b8737a44666e6903d1a77e7e4f3367ff
Maarten <github@maartenwiegers.nl>
2021-12-06 13:59:49 +0100

    Day 4 extra large tests

--- src/test/java/nl/maartenwiegers/aoc2021/Day4Test.java
+++ src/test/java/nl/maartenwiegers/aoc2021/Day4Test.java
@@ -18,8 +18,26 @@ public class Day4Test {
         assertEquals(26878, day4.solveBingoGetLastWin(5));
     }
 
-    public void shouldFindCorrectAnswerForDay4Part1WithGrid100() {
-        assertEquals(0, day4.solveBingoGetFirstWin(100));
+    // Large datasets from https://the-tk.com/project/aoc2021-bigboys.html
+
+    @Test
+    public void shouldFindCorrectAnswerForDay4Part1WithGrid15() {
+        assertEquals(22010880, day4.solveBingoGetFirstWin(15));
+    }
+
+    @Test
+    public void shouldFindCorrectAnswerForDay4Part2WithGrid15() {
+        assertEquals(5371020, day4.solveBingoGetLastWin(15));
+    }
+
+    @Test
+    public void shouldFindCorrectAnswerForDay4Part1WithGrid30() {
+        assertEquals(1527513658, day4.solveBingoGetFirstWin(30));
+    }
+
+    @Test
+    public void shouldFindCorrectAnswerForDay4Part2WithGrid30() {
+        assertEquals(199953180, day4.solveBingoGetLastWin(30));
     }
 
 }
