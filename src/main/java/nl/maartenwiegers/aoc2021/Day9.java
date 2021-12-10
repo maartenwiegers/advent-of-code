4935aed97b9fd36d85ab944f67ab3289067d4070
Maarten Wiegers <github@maartenwiegers.nl>
2021-12-10 22:50:42 +0100

    I said no wildcard imports!

--- src/main/java/nl/maartenwiegers/aoc2021/Day9.java
+++ src/main/java/nl/maartenwiegers/aoc2021/Day9.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 
 @RestController
 @Slf4j
