31ef475a091183064e48fe844e82065386f1aab9
Maarten <github@maartenwiegers.nl>
2021-12-13 23:38:27 +0100

    Removed oversize tests

--- src/test/java/nl/maartenwiegers/aoc2021/Day5Test.java
+++ src/test/java/nl/maartenwiegers/aoc2021/Day5Test.java
@@ -17,22 +17,4 @@ public class Day5Test {
     public void shouldFindCorrectAnswerForDay5Part2() {
         assertEquals(21812, day5.getCountIntersectionsWithVertical(1000));
     }
-
-    @Test
-    public void shouldFindCorrectAnswerForDay5Part1GridSize6400() {
-        assertEquals(6530681, day5.getCountIntersectionsWithoutVertical(6400));
-    }
-
-    public void shouldFindCorrectAnswerForDay5Part2GridSize6400() {
-        assertEquals(9039258, day5.getCountIntersectionsWithVertical(6400));
-    }
-
-    @Test
-    public void shouldFindCorrectAnswerForDay5Part1GridSize10000() {
-        assertEquals(30405812, day5.getCountIntersectionsWithoutVertical(10000));
-    }
-
-    public void shouldFindCorrectAnswerForDay5Part2GridSize10000() {
-        assertEquals(39512010, day5.getCountIntersectionsWithVertical(10000));
-    }
 }
