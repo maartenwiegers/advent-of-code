d2aa29a15f9453592390be266b6a77c5b491de7b
Maarten <github@maartenwiegers.nl>
2021-12-16 00:15:29 +0100

    Refactor

--- src/main/java/nl/maartenwiegers/aoc2021/Day14.java
+++ src/main/java/nl/maartenwiegers/aoc2021/Day14.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.LongSummaryStatistics;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -20,41 +21,14 @@ import java.util.stream.Collectors;
 public class Day14 {
     private static final String FILE_NAME = "input/14-%s.txt";
     private final Map<String, String> insertionRules = new HashMap<>();
-    private Map<String, Long> pairCount = new HashMap<>();
+    private Map<String, Long> characterCounts = new HashMap<>();
     private String polymer;
 
     @GetMapping("day14/score/{filename}/{steps}")
     public long getScore(@PathVariable String filename, @PathVariable int steps) {
-        simulatePolymer(filename, steps);
-        return getScore();
-    }
-
-    private void simulatePolymer(String filename, int steps) {
         initialize(filename);
-
-        Map<String, Long> pairToInsert = new HashMap<>();
-        for (int i = 0; i < polymer.length() - 1; i++) {
-            pairToInsert.compute(polymer.substring(i, i + 2), (k, v) -> v == null ? 1 : v + 1);
-        }
-        pairCount = polymer.chars()
-                .mapToObj(Character::toString)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
-        for (int i = 1; i <= steps; i++) {
-            Map<String, Long> newPairsToInsert = new HashMap<>(pairToInsert);
-            for (Map.Entry<String, Long> e : pairToInsert.entrySet()) {
-                String insertion = insertionRules.get(e.getKey());
-                newPairsToInsert.compute(e.getKey(), (k, v) -> v - e.getValue());
-                BiFunction<String, Long, Long> increase = (k, v) -> e.getValue() + (v == null ? 0 : v);
-                newPairsToInsert.compute(e.getKey()
-                        .charAt(0) + insertion, increase);
-                newPairsToInsert.compute(insertion + e.getKey()
-                        .charAt(1), increase);
-                pairCount.compute(insertion, increase);
-            }
-            pairToInsert = newPairsToInsert;
-            log.info("Finished step {}", i);
-        }
+        simulatePolymer(steps);
+        return getScore();
     }
 
     private void initialize(String filename) {
@@ -68,8 +42,34 @@ public class Day14 {
         log.info("Initialized insertions: {}", insertionRules);
     }
 
+    private void simulatePolymer(int steps) {
+        Map<String, Long> currentPairs = new HashMap<>();
+        for (int i = 0; i < polymer.length() - 1; i++) {
+            currentPairs.compute(polymer.substring(i, i + 2), (k, v) -> v == null ? 1 : v + 1);
+        }
+        characterCounts = polymer.chars()
+                .mapToObj(Character::toString)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        for (int i = 1; i <= steps; i++) {
+            Map<String, Long> newPairs = new HashMap<>(currentPairs);
+            for (Entry<String, Long> currentPair : currentPairs.entrySet()) {
+                String insertion = insertionRules.get(currentPair.getKey());
+                newPairs.compute(currentPair.getKey(), (k, v) -> v - currentPair.getValue());
+                BiFunction<String, Long, Long> increase = (k, v) -> currentPair.getValue() + (v == null ? 0 : v);
+                newPairs.compute(currentPair.getKey()
+                        .charAt(0) + insertion, increase);
+                newPairs.compute(insertion + currentPair.getKey()
+                        .charAt(1), increase);
+                characterCounts.compute(insertion, increase);
+            }
+            currentPairs = newPairs;
+            log.info("Finished step {}", i);
+        }
+    }
+
     private long getScore() {
-        LongSummaryStatistics stats = pairCount.values()
+        LongSummaryStatistics stats = characterCounts.values()
                 .stream()
                 .mapToLong(l -> l)
                 .summaryStatistics();
