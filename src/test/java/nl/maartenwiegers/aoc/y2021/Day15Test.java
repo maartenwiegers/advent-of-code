28d344a9f86c720978e5379662f28cb2d6b65242
Maarten <github@maartenwiegers.nl>
2022-02-21 22:45:20 +0100

    2020d1 completed

--- src/test/java/nl/maartenwiegers/aoc/y2021/Day15Test.java
+++ src/test/java/nl/maartenwiegers/aoc/y2021/Day15Test.java
@@ -1,6 +1,5 @@
 package nl.maartenwiegers.aoc.y2021;
 
-import nl.maartenwiegers.aoc.y2021.Day15;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
